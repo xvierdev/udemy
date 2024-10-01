ca1746ec34b04ba75908d025e3fa2713aebf9ae6
= <=>
2024-10-01 13:12:36 -0300

    :recycle: remove: delete duplicate files into folder exercices

--- src/exercices/imc2.py
+++
@@ -1,8 +0,0 @@
-# Calculo do imc
-# formula = peso / altura ** 2
-
-peso = float(input('Digite o peso em quilos: '))
-altura = float(input('Digite a altura em metros: '))
-imc = peso / altura ** altura
-
-print(f'O imc é de {imc:.2f}')
