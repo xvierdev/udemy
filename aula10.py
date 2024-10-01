bf4b8b6b2fafdcb1ea2facc6ec5306691dc72d01
= <=>
2024-10-01 12:50:44 -0300

    :construction: feat: add atividades de aulas concluídas

---
+++ aula10.py
@@ -0,0 +1,7 @@
+concatenacao = 'Wesley' + ' ' + 'Xavier'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_nome = 3 * 'Wesley '
+print(a_dez_vezes)
+print(tres_vezes_nome)
---
+++ aula9.py
@@ -0,0 +1,15 @@
+adicao = 10 + 10
+subtracao = 10 - 5
+multiplicacao = 5 * 4
+divisao = 10 / 2.2  # Divisão retornará float
+divisao_inteira = 10 // 2.2 # Divisão retornará int
+exponenciacao = 2**10
+modulo = 25 % 5  # Resto da divisão
+
+print('Adição:', adicao)
+print('Subtração:', subtracao)
+print('Multiplicação:', multiplicacao)
+print('Divisão:', divisao)
+print('Divisão inteira:', divisao_inteira)
+print('Exponenciação:', exponenciacao)
+print('Módulo:', modulo)
